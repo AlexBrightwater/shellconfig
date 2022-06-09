33662936d656b0192c0a0e13a80e6d0d45d61818
Alexander <90156402+AlexBrightwater@users.noreply.github.com>
2022-06-09 20:15:59 +0200

    Update shellconfig.sh

--- shellconfig.sh
+++ shellconfig.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
-if [[ ! -d ~/powerleevl10k ]]
+if [[ ! -d ~/powerlevel10k ]]
 then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-	sleep 5
-	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 else
 	echo "p10k already exists"
 fi
 
-if [[ ! -d ~/priv/repos/shellconfig ]]
-then
-	git clone https://github.com/AlexBrightwater/shellconfig.git ~/priv/repos/shellconfig
-	sleep 5
-else
-	cd ~/priv/repos/shellconfig
+if [[ -d ~/priv/repos/shellconfig ]]; then
+	cd ~/priv/repos/shellconfig/
 	git pull ~/priv/repos/shellconfig
+	
+	cat ~/priv/repos/shellconfig/zshrc > ~/.zshrc
+	cat ~/priv/repos/shellconfig/bash_aliases > ~/.bash_aliases
+	cat ~/priv/repos/shellconfig/p10k.zsh > ~/.p10k.zsh
+	cat ~/priv/repos/shellconfig/vimrc > ~/.vimrc
+	echo "Finished populating config files from repo in ~/priv/repos/shellconfig/."
+elif [[ -d ~/Repositories/shellconfig ]]; then
+	cd ~/Repositories/shellconfig/
+	git pull ~/Repositories/shellconfig
+	
+	cat ~/Repositories/shellconfig/zshrc > ~/.zshrc
+	cat ~/Repositories/shellconfig/bash_aliases > ~/.bash_aliases
+	cat ~/Repositories/shellconfig/p10k.zsh > ~/.p10k.zsh
+	cat ~/Repositories/shellconfig/vimrc > ~/.vimrc
+	echo "Finished populating config files from repo in ~/Repositories/shellconfig/."
+elif [[ -d ~/.shellconfig ]]; then
+	cd ~/.shellconfig/
+	git pull ~/.shellconfig
+	
+	cat ~/.shellconfig/zshrc > ~/.zshrc
+	cat ~/.shellconfig/bash_aliases > ~/.bash_aliases
+	cat ~/.shellconfig/p10k.zsh > ~/.p10k.zsh
+	cat ~/.shellconfig/vimrc > ~/.vimrc
+	echo "Finished populating config files from repo in ~/.shellconfig/."
 fi
-
-cat ~/priv/repos/shellconfig/zshrc > ~/.zshrc
-cat ~/priv/repos/shellconfig/bash_aliases > ~/.bash_aliases
-cat ~/priv/repos/shellconfig/p10k.zsh > ~/.p10k.zsh
-cat ~/priv/repos/shellconfig/vimrc > ~/.vimrc
-
