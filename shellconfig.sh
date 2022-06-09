43bcf482caed88ec650710706e09df6c970b1602
AlexBrightwater <alexander.brightwater@gmail.com>
2022-06-09 21:11:37 +0200

    Rewritten shellconfig.sh

--- shellconfig.sh
+++ shellconfig.sh
@@ -9,28 +9,30 @@ fi
 if [[ -d ~/priv/repos/shellconfig ]]; then
 	cd ~/priv/repos/shellconfig/
 	git pull ~/priv/repos/shellconfig
-	
-	cat ~/priv/repos/shellconfig/zshrc > ~/.zshrc
-	cat ~/priv/repos/shellconfig/bash_aliases > ~/.bash_aliases
-	cat ~/priv/repos/shellconfig/p10k.zsh > ~/.p10k.zsh
-	cat ~/priv/repos/shellconfig/vimrc > ~/.vimrc
+	path='~/priv/repos/shellconfig'
 	echo "Finished populating config files from repo in ~/priv/repos/shellconfig/."
 elif [[ -d ~/Repositories/shellconfig ]]; then
 	cd ~/Repositories/shellconfig/
 	git pull ~/Repositories/shellconfig
-	
-	cat ~/Repositories/shellconfig/zshrc > ~/.zshrc
-	cat ~/Repositories/shellconfig/bash_aliases > ~/.bash_aliases
-	cat ~/Repositories/shellconfig/p10k.zsh > ~/.p10k.zsh
-	cat ~/Repositories/shellconfig/vimrc > ~/.vimrc
-	echo "Finished populating config files from repo in ~/Repositories/shellconfig/."
+	path='~/Repositories/shellconfig'
+	echo "Finished populating config files from repo in ~/Repositories/shellconfig/." 
 elif [[ -d ~/.shellconfig ]]; then
 	cd ~/.shellconfig/
 	git pull ~/.shellconfig
-	
-	cat ~/.shellconfig/zshrc > ~/.zshrc
-	cat ~/.shellconfig/bash_aliases > ~/.bash_aliases
-	cat ~/.shellconfig/p10k.zsh > ~/.p10k.zsh
-	cat ~/.shellconfig/vimrc > ~/.vimrc
+	path='~/.shellconfig'
 	echo "Finished populating config files from repo in ~/.shellconfig/."
 fi
+
+cat $path/zshrc > ~/.zshrc
+cat $path/bash_aliases > ~/.bash_aliases
+cat $path/p10k.zsh > ~/.p10k.zsh
+cat $path/vimrc > ~/.vimrc
+
+
+while getopts ":s" option; do
+   case $option in
+	s) cat $path/shart_ssh.sh > ~/.start_ssh.sh;;
+  	\?) echo "Error: Didn't copy start_ssh.sh anywhere.";;  
+   esac
+done
+
