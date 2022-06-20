ea25a2e35ff5b1d4428c1b7dd1466a8b93eb79aa
AlexBrightwater <alexander.brightwater@gmail.com>
2022-06-20 20:37:52 +0200

    Fix adding restore option in shellconig.sh

--- shellconfig.sh
+++ shellconfig.sh
@@ -20,11 +20,11 @@ if [[ ! -d $conf_path/.BAK/$curr_time ]]; then
 	cp ~/.p10k.zsh $conf_path/.BAK/$curr_time/
 	cp ~/.vimrc $conf_path/.BAK/$curr_time/
 
-	echo "Backup created in $conf_path/.BAK/$curr_time/ \n"
+	echo "Backup created in $conf_path/.BAK/$curr_time/"
 	
 else 
 	echo "Could't create Backup, please try again in 1 minute."
-	exit
+	exit 1
 fi
 }
 
@@ -36,9 +36,9 @@ function install () {
 if [[ ! -d ~/powerlevel10k ]]
 then
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-	echo "cloned p10k files to ~/powerlevel10k \n"
+	echo "Cloned p10k files to ~/powerlevel10k"
 else
-	echo "p10k files already exist \n"
+	echo "Files for p10k already exist"
 fi
 
 cp $conf_path/zshrc ~/.zshrc
@@ -54,36 +54,35 @@ echo "Finished populating config files from $conf_path to home dir."
 # Restore from backup  #
 ########################
 function restore_promt () {
-	read -p "Continue do you really want to restore your current dot files? (y/n)" choice
+	read -p "Do you really want to continiue with the restore from backup? (y/n) " choice
 	case "$choice" in 
-  		y|Y ) echo "yes";;
-  		n|N ) echo "no";;
-  		* ) echo "invalid";;
+  		y|Y ) echo "Continuing with the restore process"; restore_do;;
+  		n|N ) echo "Aborting the restore process"; exit 0;;
+  		* ) echo "The answer was invalid. Please try again"; restore_promt ;;
+		
 	esac
 }
 
 function restore_do () {
-	latest=$(ls -td $conf_path/.BAK/$curr_time/* | head -1)
-	print "$latest"
-	#for file in "$latest"/*; do
-	#	cp $conf_path/BAK/$latest/.zshrc ~/.zshrc
-	#	cp $conf_path/BAK/$latest/.bash_aliases ~/.bash_aliases  
-	#	cp $conf_path/BAK/$latest/.p10k.zsh ~/.p10k.zsh
-	#	cp $conf_path/BAK/$latest/.vimrc ~/.vimrc
-	#done
+	latest=$(ls -td $conf_path/.BAK/* | head -1)
+	cp $latest/.zshrc ~/.zshrc
+	cp $latest/.bash_aliases ~/.bash_aliases
+	cp $latest/.p10k.zsh ~/.p10k.zsh
+	cp $latest/.vimrc ~/.vimrc
 	
+	echo "Restore complete"
+	exit 0
 }
 
 ###############################################
 # get options and run the specified functions #
 ###############################################
-
 while getopts ":iborh" option; do
    case $option in
 	i)	backup; install;;
 	b)	backup;;
 	o)	install;;
-	r)	restore_promt; restore_do;;
+	r)	restore_promt;;
 	h)	echo "help not implemented yet";;
   	\?)	echo "Error: Didn't copy start_ssh.sh anywhere.";;  
    esac
